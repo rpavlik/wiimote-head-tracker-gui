6b3ae1b25dcae891424bbbb44d68bf437df1818e
Ryan Pavlik <rpavlik@iastate.edu>
2010-05-18 19:23:38 -0500

    Add components header file

---
+++ src/SystemComponents.h
@@ -0,0 +1,34 @@
+/** @file	SystemComponents.h
+	@brief	header
+
+	@date	2010
+
+	@author
+	Ryan Pavlik
+	<rpavlik@iastate.edu> and <abiryan@ryand.net>
+	http://academic.cleardefinition.com/
+	Iowa State University Virtual Reality Applications Center
+	Human-Computer Interaction Graduate Program
+*/
+
+#pragma once
+#ifndef _SYSTEMCOMPONENTS_H
+#define _SYSTEMCOMPONENTS_H
+
+// Internal Includes
+// - none
+
+// Library/third-party includes
+// - none
+
+// Standard includes
+// - none
+
+enum Component {
+	CMP_CONNECTION,
+	CMP_WIIMOTE,
+	CMP_TRACKER,
+	CMP_CLIENT
+};
+
+#endif // _SYSTEMCOMPONENTS_H
