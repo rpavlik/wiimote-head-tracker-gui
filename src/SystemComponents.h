2f23f564fcd1f2038f9d492804b6ef2549a69d7d
Ryan Pavlik <rpavlik@iastate.edu>
2010-05-19 18:23:13 -0500

    Add copyright and license notices to files

--- src/SystemComponents.h
+++ src/SystemComponents.h
@@ -9,7 +9,13 @@
 	http://academic.cleardefinition.com/
 	Iowa State University Virtual Reality Applications Center
 	Human-Computer Interaction Graduate Program
-*/
+ */
+/*
+ Copyright Iowa State University 2009-2010
+ Distributed under the Boost Software License, Version 1.0.
+ (See accompanying file LICENSE_1_0.txt or copy at
+ http://www.boost.org/LICENSE_1_0.txt)
+ */
 
 #pragma once
 #ifndef _SYSTEMCOMPONENTS_H
--- src/WiimoteTrackerView.cpp
+++ src/WiimoteTrackerView.cpp
@@ -8,7 +8,13 @@
 	Iowa State University
 	Virtual Reality Applications Center and
 	Human-Computer Interaction Graduate Program
-*/
+ */
+/*
+ Copyright Iowa State University 2009-2010
+ Distributed under the Boost Software License, Version 1.0.
+ (See accompanying file LICENSE_1_0.txt or copy at
+ http://www.boost.org/LICENSE_1_0.txt)
+ */
 
 // Internal Includes
 #include "WiimoteTrackerView.h"
